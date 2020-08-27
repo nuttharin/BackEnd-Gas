5a61e4b09d907405592e77788268c4ff466ccd08
nut_tharin <37585893+nuttharin@users.noreply.github.com>
2020-08-28 01:16:54 +0700

    setup

--- app.js
+++ app.js
@@ -12,3 +12,5 @@ app.get('/',(req , res , next) =>{  // path /
     })
 
 })
+
+module.exports = app ;
--- server.js
+++ server.js
@@ -1,38 +1,14 @@
-// const https = require('https');
-// const app = require('./app');
-
-
-// // https.createServer(function (req, res) {
-// //     res.writeHead(200, {'Content-Type': 'text/plain'});
-// //     res.write('Hello World!');
-// //     res.end();
-// // }).listen(8080);
-
-// const port = process.env.PORT || 8080 ;
-
-// const server = https.createServer(app)
 
 
 
-// server.listen(port , function(){
-//     console.log('Starting node.js on port ' + port)
-// })
 
-const http = require('http')
-
+const http = require('http');
 const app = require('./app');
 
-
-const port = process.env.PORT || 8080
-
-const server = http.createServer(app)
-
-// server.on('listening',function(){
-//     console.log('ok, server is running');
-// });
-
+const port = process.env.PORT || 8080;
+const server = http.createServer(app);
 
 
 server.listen(port , function(){
-    console.log('Starting node.js on port ' + port)
+    console.log('Starting node.js on port ' + port);
 })
