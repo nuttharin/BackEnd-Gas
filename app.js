c062f9b23c033fe17e8f2c710b1216fe6bfbd4cf
nut_tharin <37585893+nuttharin@users.noreply.github.com>
2020-09-01 16:08:32 +0700

    test db

--- app.js
+++ app.js
@@ -1,16 +1,55 @@
 const express = require('express');
 const app  = express();
-//const morgan = require('morgan')
+
+const compression = require('compression');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const mongoClient = require('mongodb');
+const moment = require('moment');
 
 
-//app.use(morgan('dev')); // เเสดงการทำงาน
+require('dotenv').config()
 
-app.get('/',(req , res , next) =>{  // path /
 
-    res.status(200).json({
-        message : 'Get rootdd /'
-    })
 
+
+app.use(morgan('dev')); // เเสดงการทำงาน
+
+
+app.use(bodyParser.urlencoded({extended : false})); // false ใช้อัลกอในการ map json ธรรมดา ,true = high
+app.use(bodyParser.json());
+app.use(compression());
+
+//---------------Access-Control-Allow-Origin----------
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Orgin, X-Requested-With, Content-Type, Accept,Authorization,authorization,content-type');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next(); // ไปต่อ
 })
+
+
+
+//set Router
+const testRouter = require('./routes/testRoutes');
+
+//set path 
+
+app.use('/test',testRouter);
+
+// app.get('/',(req , res , next) =>{  // path /
+
+//     res.status(200).json({
+//         message : 'Get rootdd /'
+//     })
+
+// })
+
+
+//set path
 
 module.exports = app ;
