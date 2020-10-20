e685b1363dc3604750c2e7ebe4385e9dc17f328c
nut_tharin <37585893+nuttharin@users.noreply.github.com>
2020-10-20 14:40:27 +0700

    d

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 const http = require('http') ;
 const app = require('./app') ;
 
-const port = process.env.PORT || 8080 ;
+const port = process.env.PORT || 8000 ;
 const server = http.createServer(app) ;
 
 
