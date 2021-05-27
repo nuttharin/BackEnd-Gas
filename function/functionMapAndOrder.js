76c6c775eb5840996e454a35a7eaabee68e2ba69
nut_tharin <37585893+nuttharin@users.noreply.github.com>
2021-05-27 14:10:19 +0700

    Update functionMapAndOrder.js

--- function/functionMapAndOrder.js
+++ function/functionMapAndOrder.js
@@ -179,7 +179,8 @@ funFindDriverNearest = async (dataOrderMachine,distanceFix) => {
 
         sql = `SELECT tb_rider.id as driver_id , lat , lon FROM tb_rider 
         WHERE tb_rider."isDelete" = 0 AND tb_rider."statusWork" = 1 
-        AND NOT tb_rider.id IN(SELECT DISTINCT tb_order_send_driver.driver_id FROM tb_order_send_driver)
+        AND NOT tb_rider.id IN(SELECT DISTINCT tb_order_send_driver.driver_id 
+            FROM tb_order_send_driver WHERE tb_order_send_driver.status = 1)
         `;
         pool.query(
             sql, 
